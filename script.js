3473d7b15dd098fe1d2d579fbb846b5189a506cd
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-16 20:15:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const accessKey = "z39HRap4Z-5k5UpCu9RTgMyRXIPugcJbg44RCPyY1w8"
 
 const formE1 = document.querySelector("form")
 const inputE1 = document.getElementById("search-input")
+const searchResult = document.querySelector(".search-result")
+const showMore = document.getElementById("show-more")
