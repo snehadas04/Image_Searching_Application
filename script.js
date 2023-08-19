ba5652fd0de883752858eec807baf140df3c41f6
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-08-19 21:40:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const accessKey = "z39HRap4Z-5k5UpCu9RTgMyRXIPugcJbg44RCPyY1w8"
+const accessKey = "z39HRap4Z-5k5UpCu9RTgMyRXIPugcJbg44RCPyY1w8";
 
-const formE1 = document.querySelector("form")
-const inputE1 = document.getElementById("search-input")
-const searchResult = document.querySelector(".search-result")
-const showMore = document.getElementById("show-more")
+const formE1 = document.querySelector("form");
+const inputE1 = document.getElementById("search-input");
+const searchResult = document.querySelector(".search-result");
+const showMore = document.getElementById("show-more");
 
-let inputData = ""
+let inputData = "";
 let page = 1;
 
 async function searchInput(){
     inputData = inputE1.view;
-    const url = `https://api.unsplash.com/search/photos/pages=$(page)&query=${inputData}&client_id=${accessKey}`
+    const url = `https://api.unsplash.com/search/photos/pages=$(page)&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url)
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
 
     const result = data.result
 
